73e1488351c5d626f2ed8a870f649a118d0a71fa
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-10-31 10:17:14 -0400

    Age

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age"))
+
+if age < 5:
+    print ("play with your toys")
+elif age >= 5 and age <= 12:
+        print("go play some video games")
+elif age >=13 and age <=17:
+        print ("go outside")
+else:
+    print("go get a job")
